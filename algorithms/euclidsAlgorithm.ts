869570863623a48a1de4a9a08e45d4c459d66e1c
David L. Bowman <david@theinnovationlab.dev>
2025-03-28 15:20:23 -0500

    comments

--- algorithms/euclidsAlgorithm.ts
+++ algorithms/euclidsAlgorithm.ts
@@ -13,3 +13,6 @@ const secondNum = Number.parseInt(prompt("Enter the second number: ") || "0");
 console.log(
 	`The GCD of ${firstNum} and ${secondNum} is: ${getGCD(firstNum, secondNum)}`,
 );
+
+/* A little intuition for why this works. When you divide m by n, you get a quotient and a remainder. You could write this as (m % n) * n + r = m. When r > 0, the GCD must be smaller than n, but both the n and the remainder must be divisible by the GCD. We recursively reduce the problem until we get a remainder of 0, at which point we have found the GCD, being the last non-zero remainder.
+ */
